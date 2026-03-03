36387f4e005d7c04bba8712217b17340722065bc
CFRA204 <c.frantzen@comcave.com>
2026-03-03 09:45:22 +0100

    Constructor added

--- src/Lama.java
+++ src/Lama.java
@@ -3,6 +3,6 @@ public class Lama{
     private String tierlaut = "Möööht";
 
     public Lama(){
-        System.out.println("Lama erstellt"); 
+        System.out.println("Lama erstellt");
     }
 }
